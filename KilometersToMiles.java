0e4621c68254d6f6c02a35aa6216a2896c0431b8
Sushant-Kumar19 <sushantk049@gmail.com>
2025-06-13 09:16:37 +0530

    changes

--- KilometersToMiles.java
+++
@@ -1,7 +0,0 @@
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers / 1.6;
-        System.out.printf("The distance %.1f km in miles is %.2f%n", kilometers, miles);
-    }
-}
