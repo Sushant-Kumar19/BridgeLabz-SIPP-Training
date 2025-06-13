5b5d5dc595b154500dc6779480e48510610bfac3
Sushant-Kumar19 <sushantk049@gmail.com>
2025-06-13 08:33:23 +0530

    rest question assign 1

---
+++ day-1/TotalPurchasePrice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TotalPurchasePrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter unit price: ");
+        double unitPrice = scanner.nextDouble();
+        System.out.print("Enter quantity: ");
+        int quantity = scanner.nextInt();
+        double total = unitPrice * quantity;
+        System.out.printf("The total purchase price is INR %.2f if the quantity %d and the unit price is INR %.2f\n", total, quantity, unitPrice);
+        scanner.close();
+    }
+} 
