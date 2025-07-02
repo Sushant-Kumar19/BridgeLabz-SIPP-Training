f254e73c2c55eb23de3097a077ed7858b00e01c9
Sushant-Kumar19 <sushantk049@gmail.com>
2025-07-02 12:43:25 +0530

    Sushant Kumar Cluster 2 Section N

--- Day1_ProgrammingElements/Answer.java
+++
@@ -1,9 +0,0 @@
-package Day1_ProgrammingElements;
-
-public class Answer {
-
-    public static void main(String[] var0) {
-        int age = 2024 - 2000;
-        System.out.println("harry's age in 2024 is " + age);
-    }
-}
